77d823d95a5554c416fd07caf9b636476147a892
Tadeo <teoleon2013@gmail.comgit config --global user.name Tadeogit config --global user.email teoleon2013@gmail.com>
2021-03-02 19:41:33 -0300

    agregue el footer, me falta lograr que el BP de 320-638 funcione

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-            '2xl': '1536px',
+            'xs': { 'min': '320px', 'max': '638px' },
+            'sm': { 'min': '640px', 'max': '767px' },
+            'md': { 'min': '768px', 'max': '1023px' },
+            'lg': { 'min': '1024px', 'max': '1279px' },
+            'xl': { 'min': '1280px', 'max': '1535px' },
+            '2xl': { 'min': '1536px' },
         },
         colors: {
             transparent: 'transparent',
